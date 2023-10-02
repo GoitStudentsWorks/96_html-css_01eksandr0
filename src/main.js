39b485da0abd1c249900fc108284612ddddb355c
Anton <asmeiko13@gmail.com>
2023-10-02 10:39:24 +0200

    add-js-for-btn

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-open-menu').onclick = function() {
+  document.getElementById('mobil-menu-burger').classList.add('is-open');
+}
+document.getElementById('btn-close-burger').onclick = function() {
+  document.getElementById('mobil-menu-burger').classList.remove('is-open');
+}
