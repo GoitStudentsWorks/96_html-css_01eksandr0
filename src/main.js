6dfe1d8298cffd84831cb6acc47ce1c04367f721
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-10-05 10:58:49 +0300

    +corect code js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-document.getElementById('btn-open-menu').onclick = function() {
-  document.getElementById('mobil-menu-burger').classList.add('is-open');
-}
-document.getElementById('btn-close-burger').onclick = function() {
-  document.getElementById('mobil-menu-burger').classList.remove('is-open');
-}
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('#btn-open-menu'),
+    closeModalBtn: document.querySelector('#btn-close-burger'),
+    modal: document.querySelector('#mobil-menu-burger'),
+    closeMenuLink: document.querySelectorAll('.mobile-link'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeMenuLink.forEach(item =>
+    item.addEventListener('click', toggleModal)
+  );
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
